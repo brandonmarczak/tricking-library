e4e1d185e431bf4fa129f5bdc00a5a975f5f91e1
Brandon <brandonmarczak@yahoo.co.uk>
2020-11-16 08:03:33 +0000

    init

---
+++ Controllers/Home.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World!!!!";
+        }
+    }
+}
