29dc75ca306a859611d80361ff1c9ce5fc9aa4da
Brandon <brandonmarczak@yahoo.co.uk>
2021-01-16 21:35:31 +0000

    vue files cleanup

--- web-client/pages/index.vue
+++ web-client/pages/index.vue
@@ -1,39 +1,26 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <div v-if="tricks">
-          <p v-for="t in tricks">{{ t.name }}</p>
-        </div>
-        <div>
-          <v-text-field label="Tricking Name" v-model="trickName"></v-text-field>
-          <v-btn @click="saveTrick">Save Trick</v-btn>
-        </div>
-        <v-card-title class="headline">
-          {{ message }}
-          <v-btn @click="reset">Reset Message</v-btn>
-          <v-btn @click="resetTricks">Reset Tricks</v-btn>
-        </v-card-title>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+
+    <div v-if="tricks">
+      <p v-for="t in tricks">{{ t.name }}</p>
+    </div>
+
+    <div>
+      <v-text-field label="Tricking Name" v-model="trickName"></v-text-field>
+      <v-btn @click="saveTrick">Save Trick</v-btn>
+    </div>
+
+    {{ message }}
+    <v-btn @click="reset">Reset Message</v-btn>
+    <v-btn @click="resetTricks">Reset Tricks</v-btn>
+  </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
 import Axios from "axios";
 import { mapState, mapActions, mapMutations } from "vuex";
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  },
   data: () => ({
     trickName: ""
   }),
@@ -52,7 +39,7 @@ export default {
     }),
     ...mapActions("tricks", ["createTrick"]),
     async saveTrick() {
-      await this.createTrick({trick: {name: this.trickName}});
+      await this.createTrick({ trick: { name: this.trickName } });
       this.trickName = "";
     }
   }
